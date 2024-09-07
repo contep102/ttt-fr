1e98e2897bcbca98c8c3f4a559c084d643e23efb
Cov001 <cov6155@gmail.com>
2024-09-07 23:42:11 +0700

    j

--- src/AgoraSetup.js
+++ src/AgoraSetup.js
@@ -1,13 +1,16 @@
-import { createClient, createMicrophoneAndCameraTracks, createScreenVideoTrack } from "agora-rtc-react";
+import {
+  createClient,
+  createMicrophoneAndCameraTracks,
+  createScreenVideoTrack,
+} from "agora-rtc-react";
 
-import AgoraRTM from 'agora-rtm-sdk';
+import AgoraRTM from "agora-rtm-sdk";
 
-const appId = "9018a94d10f34248aa03f0d14a011fb9";
+const appId = "40901906ee3044809071083e04367ae7";
 const token = null;
 export const config = { mode: "rtc", codec: "vp8", appId: appId, token: token };
 export const useClient = createClient(config);
 export const useMicrophoneAndCameraTracks = createMicrophoneAndCameraTracks();
 export const useScreenVideoTrack = createScreenVideoTrack();
-
 
 export const rtmClient = AgoraRTM.createInstance(appId);
